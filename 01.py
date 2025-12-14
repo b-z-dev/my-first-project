6d8bee9dbe1c023bf58cf85e86ae82216bf018cf
b-z-dev <bahar.zand.dev@gmail.com>
2025-12-14 00:10:44 -0800

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,2 +1,37 @@
 # my-first-project
 print("hello world")
+x = 5
+y = 3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x**y)
+print(x/y)
+print(x//y)
+print(x%y)
+x = "my name is 'bahar'"
+print(x)
+c = "hello\nworld"
+print(c)
+y = "my name is \"bahar\""
+print(y)
+n = "hello\bworld"
+print(n)
+z = "hello\tworld"
+print(z)
+b = """hello world
+my name is   \"bahar\" """
+print(b)
+x = "hello world"
+print(x[1:-2])
+name = "bahar"
+lastname = "zand"
+age = 18
+m = f"hello my name is {name} and my last name is {lastname} i am {age} years old"
+print(m)
+x = "bahar"
+print(x.capitalize())
+r = "bahar"
+print(r.upper())
+o = "BAHAR"
+print(o.casefold())
